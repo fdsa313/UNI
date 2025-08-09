36aa908976395bf6bd2e206add15d3a99ca928a2
sjieys <0wonyounglee0@gmail.com>
2025-08-09 22:17:55 +0900

    퀴즈, 비디오 CRUD 구현

--- server.js
+++ server.js
@@ -4,40 +4,52 @@ const PORT = process.env.PORT || 3000;
 const path = require('path');
 const  { notifyQueue } = require('./queue.js');
 const SECRET_KEY = 'secret_key';
+const jwt = require('jsonwebtoken');
+
+// CJS
+const { ulid, monotonicFactory } = require('ulid');
 
 app.use(express.json());
 
 // 퀴즈
 // 퀴즈 문제, 정답 주기
-app.get('/quizzes/answers/:quizId', (req, res) => {
+app.get('/quizzes/:quizId', (req, res) => { // TODO: 이쪽은 고민 중
   const quizId = req.params.quizId;
   // DB: const quiz = db.getQuiz(quizId); 
   res.status(200).json({quiz: quiz});
 });
+// 퀴즈 객체 배열 주기
+app.get('/quizzes', verifyToken, (req, res) => {
+  // DB: const quizzes = db.getAllQuizzes(req.user.userId);
+  res.status(200).json({quizzes: quizzes});
+});
 // 퀴즈 POST
-app.post('/quizzes/answers', (req, res) => {
-  const quizId = Date.now();
+app.post('/quizzes', verifyToken, (req, res) => {
+  const quizId = getQuizId();
   const imgFile = req.body.imgFile;
   const answer = req.body.answer;
   const quiz = {
+    userId: req.user.userId,
     quizId,
     imgFile,
     answer
   };
   // DB: db.saveQuiz(quiz);
+  res.status(201).json({message: "잘 추가됨", quiz});
 });
 // 퀴즈 DELETE
-app.delete('/quizzes/answers/:quizId', (req, res) => {
+app.delete('/quizzes/:quizId', (req, res) => {
   // DB: db.deletequiz(req.params.quizId);
   res.status(200).json({message: "잘 삭제됨"})
 });
 
 
 // 푸쉬토큰 등록
-app.post('/register-token', async (req, res) => {
-  const { userId, token, platform, timezone } = req.body;
+app.post('/register-token', verifyToken, async (req, res) => {
+  const userId = req.user.userId; // 인증된 사용자 ID
+  const { token, platform, timezone } = req.body;
 //   DB: await db.upsertDeviceToken({ userId, token, platform, timezone });
-  res.sendStatus(200);
+  res.status(200).json({ success: true, message: '토큰이 등록되었습니다.' });
 });
 
 // 알림 DB에 저장하는 코드
@@ -119,13 +131,56 @@ app.patch('/reminders/:id', async (req, res) => {
     res.status(500).json({ error: 'internal' });
   }
 });
+// 알림 삭제하는 코드
+app.delete('/reminders/:id', async (req, res) => {
+  const { id } = req.params;
+  // DB: db.deleteNotification(id);
+  const jobId = `notif:${id}`;
+  const job = await notifyQueue.getJob(jobId);
+  if (job) await job.remove();
 
+  res.status(200).json({ success: true, message: '알림이 삭제되었습니다.' });
+});
+
+
+// QUIZ
+app.get('/quizzes', (req, res) => {
+  const quizzes = []; // DB: db.getAllQuizzes();
+  res.status(200).json({ quizzes });
+});
+app.post('/quizzes', (req, res) => {
+  const { imgFile, answer } = req.body;
+  const quizID = getQuizId;
+  const quiz = { quizID, imgFile, answer };
+  // DB: db.addQuiz(quiz);
+  res.status(201).json({ success: true, message: '퀴즈가 추가되었습니다.', quiz });
+});
+app.delete('/quizzes/:quizId', (req, res) => {
+  // DB: db.deleteQuiz(req.params.quizId);
+  res.status(200).json({ success: true, message: '퀴즈가 삭제되었습니다.' });
+});
+
+
+// 영상
+app.get('/videos', verifyToken, (req, res) => {
+  const videos = []; // DB: db.getAllVideos(userId);
+  res.status(200).json({ videos });
+});
+app.post('/videos', verifyToken, (req, res) => {
+  const userId = req.user.userId; // 인증된 사용자 ID
+  const video = {
+    userId,
+    videoId: getVideoId(), 
+    title: req.body.title,
+    url: req.body.url,
+  }
+});
 
 // 로그인
 app.post('/login', (req, res) => {
   const id = req.body.id;
   const pw = req.body.pw;
-  const user = storage.getUsersById(id);
+  // DB: const user = db.getUsersById(id);
   // id에 해당하는 user가 없거나, 비번이 틀리거나
   if(!user || pw != user.userPw) {
     return res.status(401).json({sucess: false, message: "아이디 또는 비밀번호가 잘못 되었습니다.\n아이디와 비밀번호를 정확히 입력해 주세요."});
@@ -140,23 +195,25 @@ app.post('/login', (req, res) => {
 
   res.status(200).json({ success: true, token, id: id });
 });
+
+
 // ==============회원가입============== 
 app.post('/signup', (req, res) => {
-const {signupId, signupPw} = req.body;
+  const {signupId, signupPw} = req.body;
 
-// 신규 등록자
-if(!storage.getUsersById(signupId)) { // DB: db.getUserById(userId)
-    const user = {
-      userId: signupId,
-      userPw: signupPw
-    };
-    storage.addUser(user); // DB: db.addUser(user);
-    res.status(200).json({success: true, message:"회원가입 완료"});
-}
-else {
-    // 기존 유저
-    res.status(400).json({success: false, message: "이미 가입된 계정입니다. 로그인을 진행해 주세요!"});
-}
+  // 신규 등록자
+  if(!storage.getUsersById(signupId)) { // DB: db.getUserById(userId)
+      const user = {
+        userId: signupId,
+        userPw: signupPw
+      };
+      storage.addUser(user); // DB: db.addUser(user);
+      res.status(200).json({success: true, message:"회원가입 완료"});
+  }
+  else {
+      // 기존 유저
+      res.status(400).json({success: false, message: "이미 가입된 계정입니다. 로그인을 진행해 주세요!"});
+  }
 });
 // ME
 app.get('/me',verifyToken, (req, res) => {
@@ -165,7 +222,6 @@ app.get('/me',verifyToken, (req, res) => {
 });
 
 // VERIFY
-
 function verifyToken(req, res, next) {
   // 알아서 라우터 쪽의 req에 user정보 넣어줌
   const authHeader = req.headers.authorization;
@@ -181,6 +237,18 @@ function verifyToken(req, res, next) {
 }
 
 
+// ID 생성 함수
+function getVideoId() {
+  return 'vid_' + ulid();
+}
+function getQuizId() {
+  return 'qz_' + ulid();
+}
+
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
---
+++ test_code.js
@@ -0,0 +1,10 @@
+// CJS
+const { ulid, monotonicFactory } = require('ulid');
+
+const id1 = ulid();                 // 일반 ULID
+const nextULID = monotonicFactory(); // 같은 ms 내에서도 정렬 보장
+const id2 = nextULID();
+
+console.log('vid_' + id1);
+// console.log('qz_' + id2);
+console.log('vid_' + id1);
